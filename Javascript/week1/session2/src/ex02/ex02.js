6e2464c03d4cd319a10947ea81fefc2d2d468752
Floren Henri <fhenri@student.42.fr>
2019-03-01 19:53:59 +0100

    Update ex02.js

--- Javascript/week1/session2/src/ex02/ex02.js
+++ Javascript/week1/session2/src/ex02/ex02.js
@@ -1,27 +1,24 @@
-  /* Une function pour gérer l'addition de la calculatrice */ 
+  /* Une fonction pour gérer l'addition de la calculatrice */ 
   function add() {
     
   }
 
-  /* Une function pour gérer la soustraction de la calculatrice */ 
+  /* Une fonction pour gérer la soustraction de la calculatrice */ 
   function sub() {
     
   }
   
-  /* Une function pour gérer la multiplication de la calculatrice */ 
+  /* Une fonction pour gérer la multiplication de la calculatrice */ 
   function mul() {
     
   }
 
-  /* Une function pour gérer la division de la calculatrice */ 
+  /* Une fonction pour gérer la division de la calculatrice */ 
   function div() {
     
   } 
 
-  /* Une function pour afficher le resultats de la calculatrice */ 
+  /* Une fonction pour afficher le resultats de la calculatrice */ 
   function displayRes(resId, total) {
-    var element = document.getElementById(resId)
     
-    element.innerHTML  = total
-    return
   }
