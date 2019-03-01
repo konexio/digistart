6b1f7554600a3351be35a2752e54b5422ab446a9
Floren Henri <fhenri@student.42.fr>
2019-03-01 19:47:39 +0100

    Update ex01.js

--- Javascript/week1/session2/src/ex01/ex01.js
+++ Javascript/week1/session2/src/ex01/ex01.js
@@ -1,9 +1,9 @@
- /* Une function qui affiche une alert a chaque click */ 
+ /* Une fonction qui affiche une alerte à chaque click sur un bouton */ 
 function firstEvent() {
   
 }
 
- /* Une function qui affiche touts les caractere mis */ 
+ /* Une fonction qui affiche les caractères entrés dans une balise input */ 
 function secondEvent() {
   var inputValue = document.getElementById('inputId').value
   var displayElement = document.getElementById('resId')
@@ -12,12 +12,13 @@ function secondEvent() {
   displayElement.innerText = inputValue
 }
 
- /* Une function qui affiche une alert */ 
+ /* Une fonction qui va changer la couleur d'un élément HTML à chaque entrée de la sourie dans cet élément */ 
 function thirdEvent() {
   var square = document.getElementById('resOver')
   square.setAttribute("style", 'width:100px;height:100px;background-color:blue;');
 }
 
+/* Une fonction qui va changer la couleur d'un élément HTML à chaque sortie de la sourie dans cet élément */ 
 function fourthEvent() {
   var square = document.getElementById('resOver')
   square.setAttribute("style", 'width:100px;height:100px;background-color:red;');
